c0bade69b4d842cb5f0a0d8d9dfcf03fb868d2f5
ekenecf <ekens2u@gmail.com>
2022-08-15 19:24:07 +0100

    Fix rubocop offences

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def index
-        @users = User.all
-    end
+  def index
+    @users = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'     
- end
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root to: "users#index"
+  root to: 'users#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
