e6349f0315d9207f152f332ca6ee906b117eb9cf
ekenecf <ekens2u@gmail.com>
2022-08-16 16:00:30 +0100

    Fix rubocop offences

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
-# frozen_string_literal: true
-
-# rubocop:todo Lint/MissingCopEnableDirective
-# rubocop:disable Style/Documentation
-# rubocop:enable Lint/MissingCopEnableDirective
-
 module ApplicationHelper
 end
