02ba9b4d5a897a47f796719386174f795ca934a2
ekenecf <ekens2u@gmail.com>
2022-08-31 23:23:47 +0100

    Remove unwanted codes from my codebase

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class FoodsController < ApplicationController
   load_and_authorize_resource
 
--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class GeneralShoppingListsController < ApplicationController
   def index # rubocop:todo Metrics/AbcSize, Metrics/MethodLength
       @recipe_foods_lists = [] # rubocop:todo Layout/IndentationWidth
